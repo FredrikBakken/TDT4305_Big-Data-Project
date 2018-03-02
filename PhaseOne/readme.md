4974bdf0aa7973fb4a49efc24d8ab45f091eee9c
FredrikBakken <fredda10x@gmail.com>
2018-03-02 11:26:27 +0100

    Task 1: max/min solved.

---
+++ PhaseOne/readme.md
@@ -0,0 +1,33 @@
+# Phase One
+
+## Task 1
+
+**TODO**
+
+## Task 2
+
+**TODO**
+
+## Task 3
+
+**TODO**
+
+## Task 4
+
+**TODO**
+
+## Task 5
+
+**TODO**
+
+## Task 6
+
+**TODO**
+
+## Task 7
+
+**TODO**
+
+## Task 8
+
+**TODO**
--- PhaseOne/task_1.py
+++ PhaseOne/task_1.py
@@ -41,15 +41,18 @@ def totalNumber(tweets):
 
 
 # Count number of distinct values
-def countDistinct(data, r):
-    return data.map(lambda row: row[r]).distinct().count()
+def countDistinct(data, row):
+    return data.map(lambda x: x[row]).distinct().count()
 
 
 # Minimum value
-# TODO
+def minValue(data, row):
+    return data.min(key=lambda x : float(x[row]))[row]
+
 
 # Maximum value
-# TODO
+def maxValue(data, row):
+    return data.max(key=lambda x : float(x[row]))[row]
 
 
 # Avg. characters
@@ -67,27 +70,33 @@ def task1_1(input_file, output_file):
     data = rawData.map(lambda x: x.split('\n')[0].split('\t'))\
 
     # Processing data
-    total_tweets = totalNumber(data)
-    usernames = countDistinct(data, USERNAME)
-    country_names = countDistinct(data, COUNTRY_NAME)
-    place_names = countDistinct(data, PLACE_NAME)
-    languages = countDistinct(data, LANGUAGE)
+    #total_tweets = totalNumber(data)
+    #usernames = countDistinct(data, USERNAME)
+    #country_names = countDistinct(data, COUNTRY_NAME)
+    #place_names = countDistinct(data, PLACE_NAME)
+    #languages = countDistinct(data, LANGUAGE)
+    #minLat = minValue(data, LATITUDE)
+    #minLng = minValue(data, LONGITUDE)
+    #maxLat = maxValue(data, LATITUDE)
+    #maxLng = maxValue(data, LONGITUDE)
 
 
     # Result print
-    print('Total number of tweets: ' + str(total_tweets))
-    print('Number of distinct usernames: ' + str(usernames))
-    print('Number of distinct country names: ' + str(country_names))
-    print('Number of distinct place names: ' + str(place_names))
-    print('Number of distinct languages: ' + str(languages))
+    #print('Total number of tweets: ' + str(total_tweets))
+    #print('Number of distinct usernames: ' + str(usernames))
+    #print('Number of distinct country names: ' + str(country_names))
+    #print('Number of distinct place names: ' + str(place_names))
+    #print('Number of distinct languages: ' + str(languages))
+    #print('Minimum latitude: ' + str(minLat))
+    #print('Minimum longitude: ' + str(minLng))
+    #print('Maximum latitude: ' + str(maxLat))
+    #print('Maximum longitude: ' + str(maxLng))
 
 
     # Testing
-    #test = data.map(lambda )
-    #test = data.map(lambda row : row[TWEET_TEXT]).getAll()
-    #r = averageLen(test)
-    #print(test)
-    #print(r)
+    # ...
+
+
 
 
 
@@ -101,4 +110,4 @@ if __name__ == "__main__":
     except IndexError:
         task1_1('/data/geotweets.tsv', '/data/output_task1')
     except:
-        print('Something went wrong during the initialization.')
+        print('Something went wrong during the initialization. Please see the command execution examples on Github (www.github.com/FredrikBakken/TDT4305_Big-Data-Project/tree/master/PhaseOne).')
