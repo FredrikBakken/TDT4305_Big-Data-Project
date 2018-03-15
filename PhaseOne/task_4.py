c18a9cc3ff9e0637cd581e297e5d893735621202
Tor Arne <2rry2r@gmail.com>
2018-03-15 21:39:04 +0100

    Task 4: solved

--- PhaseOne/task_4.py
+++ PhaseOne/task_4.py
@@ -1,6 +1,5 @@
 '''
 TASK 4: Most Active Hours per Country
-
 Description:
  - Find 1-hour time interval with max. number of tweets for each country
  - Use local time for tweets
@@ -13,8 +12,20 @@ Description:
 
 import sys
 
-from prettytable import PrettyTable
-from pyspark import SparkConf, SparkContext
+from pyspark import SparkContext
+from pyspark import SparkConf
+
+from datetime import datetime
+
+
+# 1-hour time interval with max. number of tweets for each country
+def countMaxTweetsForEachCountry(data):
+    return data.map(lambda x: (x[COUNTRY_NAME], int(x[UTC_TIME]) + int(x[TIMEZONE_OFFSET])))\
+                .map(lambda x: ((x[0], datetime.fromtimestamp(x[1]/1000).hour), 1))\
+                .aggregateByKey(0, (lambda x, y: x + y), (lambda rdd1, rdd2: (rdd1+rdd2)))\
+                .map(lambda x: (x[0][0], (x[0][1], x[1])))\
+                .reduceByKey(lambda x, y: y if y[1] > x[1] else x)\
+                .map(lambda x: (x[0],x[1][0],x[1][1])).sortByKey(True,1)
 
 
 # Header structure
@@ -32,13 +43,8 @@ TWEET_TEXT          = 10
 LATITUDE            = 11
 LONGITUDE           = 12
 
-
-# Apache Spark
-# TODO
-
-
 # Task 4 main
-def task1_4():
+def task1_4(input_file, output_file):
     conf = SparkConf().setMaster('local[*]').setAppName('TDT4305: Big Data Architecture - Project Phase 1, Task 4')
     sc = SparkContext(conf = conf)
 
@@ -46,13 +52,10 @@ def task1_4():
     data = rawData.map(lambda x: x.split('\n')[0].split('\t'))\
 
     # Processing data
-    # TODO
+    result_countMaxTweetsForEachCountry = countMaxTweetsForEachCountry(data)
 
     # Store results in file
-    # TODO
-
-    # Printing results to console (not part of the requirements)
-    # TODO
+    result_countMaxTweetsForEachCountry.coalesce(1,True).saveAsTextFile(output_file)
 
 
 if __name__ == "__main__":
@@ -66,4 +69,3 @@ if __name__ == "__main__":
         task1_4('/data/geotweets.tsv', 'data/results/result_4.tsv')
     except:
         print('Something went wrong during the initialization. Please see the command execution examples on Github (www.github.com/FredrikBakken/TDT4305_Big-Data-Project/tree/master/PhaseOne).')
-
